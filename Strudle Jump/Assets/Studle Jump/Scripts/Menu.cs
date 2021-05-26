b0245c33ab3385a50a35bba4a582c655ad7f5ae6
SteveoBurns <steveo.burns.777@gmail.com>
2021-05-26 16:33:20 +1000

    Added volume sliders

--- Strudle Jump/Assets/Studle Jump/Scripts/Menu.cs
+++ Strudle Jump/Assets/Studle Jump/Scripts/Menu.cs
@@ -4,10 +4,41 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
 using UnityEngine.UI;
+using UnityEngine.Audio;
 
 public class Menu : MonoBehaviour
 {
-    [SerializeField] private Slider volumeSlider;
+    [SerializeField] private AudioMixer audioMixer;
+    [SerializeField] private GameObject pausePanel;
+    
+    private bool isPaused = false;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && !isPaused)
+        {
+            PauseGame();
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape) && isPaused)
+            Play();
+    }
+
+    public void VolumeSlider(float _volume)
+    {
+        _volume = VolumeRemap(_volume);
+        audioMixer.SetFloat("masterVolume", _volume);
+    }
+    public float VolumeRemap(float value)
+    {
+        return -40 + (value - 0) * (20 - -40) / (1 - 0);
+    }
+
+    public void PauseGame()
+    {
+        pausePanel.SetActive(true);
+        isPaused = true;
+        Time.timeScale = 0;
+    }
 
     public void LoadMain()
     {
@@ -21,6 +52,8 @@ public class Menu : MonoBehaviour
 
     public void Play()
     {
+        pausePanel.SetActive(false);
+        isPaused = false;
         Time.timeScale = 1;
     }
 
